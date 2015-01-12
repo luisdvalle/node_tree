d8c72f95d6bf05f028e7bed38c7e72122df38823
luisdvalle <luisdvalle@users.noreply.github.com>
2015-01-12 12:11:15 +1100

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,28 +1,34 @@
-public class Node 
+public class Node
 {
-  private int value;
-  private Node left;
-  private Node right;
-
-  void setLeftNodeValue(int value)
-  {
-    left.value = value;
-  }
-  
-  void setRightNodeValue(int value)
-  {
-    right.value = value;
-  }
-
-  void PrintNodeTree(node tree) 
-  {
-    if(left != null)
-      System.out.print(left.value);
-    
-    System.out.print(tree.value);
-    
-    if(right != null)
-      System.out.print(right.value);
-  }
-
+	private int value;
+	private Node left;
+	private Node right;
+	
+	Node(int value)
+	{
+		this.value = value;
+	}
+	
+	public void setLeftNode(Node left)
+	{
+		this.left = left;
+	}
+	
+	public void setRightNode(Node right)
+	{
+		this.right = right;
+	}
+	
+	public void printNodeTree()
+	{
+		System.out.print("Node Value: "+value+"\n");
+		
+		if(left != null)
+			System.out.print("\tLeft Node Value: "+left.value+"\n");
+		
+		if(right != null)
+			System.out.print("\tRight Node Value: "+right.value+"\n");
+		
+		System.out.print("\n");
+	}
 }
